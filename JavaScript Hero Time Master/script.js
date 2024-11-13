d52cd4b4b633c5ea63b23a629acceba66e93480a
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-13 09:50:10 +0000

    changed from functions to classes

--- JavaScript Hero Time Master/script.js
+++ JavaScript Hero Time Master/script.js
@@ -1,34 +1,28 @@
 const selectedHours = document.getElementById("select-hours")
+const alarmSound = document.getElementById("alarm-sound")
 
 let selectedHour = '24'
 
-function Clock(format = "24") {
-    this.format = format
-
-    // Get current time
-    const currentTime = new Date();
-    this.hours = currentTime.getHours();
-    this.minutes = currentTime.getMinutes();
-    this.seconds = currentTime.getSeconds();
-
-    // Method to format time in 24-hour format "HH:MM:SS"
-    this.getFormattedTime = function () {
-        if (this.format === '12') {
-            return this.get12HourTime();
-        }
-
-        return `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds}`;
-    };
-
-    // Method to format time as "HH:MM:SS AM/PM"
-    this.get12HourTime = function () {
+class Clock {
+    constructor(format = "24") {
+        const currentTime = new Date();
+        this.format = format
+        this.hours = currentTime.getHours();
+        this.minutes = currentTime.getMinutes();
+        this.seconds = currentTime.getSeconds();
         this.ampm = this.hours >= 12 ? 'PM' : 'AM';
+    }
+
+    getFormattedTime() {
+        return (this.format === '12') ? this.get12HourTime() : `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds}`;
+    }
+
+    get12HourTime() {
         // Convert to 12-hour format
         this.hours = this.hours % 12;
         this.hours = this.hours ? this.hours : 12; // Convert hour '0' to '12'
         return `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds} ${this.ampm}`;
-    };
-
+    }
 }
 
 const updateTime = () => {
@@ -43,4 +37,5 @@ selectedHours.addEventListener("change", (e) => {
     selectedHour = e.target.value
     updateTime()
 })
+
 
