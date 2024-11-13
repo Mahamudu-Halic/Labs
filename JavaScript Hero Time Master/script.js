fc6da7770bda316e6a1dd116c7972ece4903ffb1
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-13 13:07:05 +0000

    time master intial commit

--- JavaScript Hero Time Master/script.js
+++ JavaScript Hero Time Master/script.js
@@ -1,7 +1,8 @@
 const selectedHours = document.getElementById("select-hours")
 const alarmSound = document.getElementById("alarm-sound")
-
+const setAlarmBtn = document.getElementById("set-alarm")
 let selectedHour = '24'
+let myAlarm
 
 class Clock {
     constructor(format = "24") {
@@ -11,6 +12,7 @@ class Clock {
         this.minutes = currentTime.getMinutes();
         this.seconds = currentTime.getSeconds();
         this.ampm = this.hours >= 12 ? 'PM' : 'AM';
+        this.alarmTime = null
     }
 
     getFormattedTime() {
@@ -23,11 +25,24 @@ class Clock {
         this.hours = this.hours ? this.hours : 12; // Convert hour '0' to '12'
         return `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds} ${this.ampm}`;
     }
+
+    setAlarm(hours, minutes) {
+        this.alarmTime = {hours: parseInt(hours), minutes: parseInt(minutes), seconds: 0};
+    }
+
+    checkAlarm() {
+        if (this.alarmTime && this.hours === this.alarmTime.hours &&
+            this.minutes === this.alarmTime.minutes) {
+            alarmSound.play();  // Play the alarm sound when time matches
+            alert('Alarm Time!');  // Show an alert message
+        }
+    }
 }
 
 const updateTime = () => {
     const dateTime = new Clock(selectedHour)
     document.getElementById("display-time").textContent = dateTime.getFormattedTime();
+    dateTime.checkAlarm()
 }
 
 setInterval(() => updateTime(), 1000) // Call updateTime()
@@ -38,4 +53,15 @@ selectedHours.addEventListener("change", (e) => {
     updateTime()
 })
 
+setAlarmBtn.addEventListener("click", () => {
+    const alarmTimeInput = document.getElementById('alarm-time').value;
+    if (alarmTimeInput) {
+        const [hours, minutes] = alarmTimeInput.split(':'); // Get hours and minutes from the input
+        const myClock = new Clock();
+        myClock.setAlarm(hours, minutes); // Set the alarm time
+        alert(`Alarm set for ${hours}:${minutes}`); // Alert the user that the alarm is set
+    } else {
+        alert('Please select a valid time for the alarm!');
+    }
+})
 
