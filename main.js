05500a04cc287ecde4a8eb6f761591c8590c98e3
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-19 10:53:53 +0000

    password generator

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const lowercaseCheck = document.getElementById("lowercase-checkbox");
 const numberCheck = document.getElementById("number-checkbox");
 const symbolCheck = document.getElementById("symbol-checkbox");
 const strengthStatus = document.getElementById("strength-status");
-const passwordInputGroup = document.querySelector(".password-input-group");
 const generateBtn = document.getElementById("generate-btn");
 const bars = document.querySelectorAll(".bars div");
 
@@ -17,49 +16,60 @@ let numbers = "0123456789";
 let symbols = "!@#$%^&*()_+";
 
 const toast = {
-  toastDiv: document.querySelector("#toast"),
-  /**
-   * Show a success toast message
-   * @param {string} [message="success"] - the message to show in the toast
-   * @returns {void}
-   */
-  success(message = "success") {
-    this.toastDiv.classList.add("success");
-    this.toastDiv.innerHTML = message;
+    copyStatus: document.querySelector("#copy-status"),
+    /**
+     * Show a success copyStatus message
+     * @param {string} [message="success"] - the message to show in the copyStatus
+     * @returns {void}
+     */
+    success(message = "success") {
+        this.copyStatus.style.color = "#A4FFAF"
+        this.copyStatus.textContent = message;
 
-    setTimeout(() => {
-      this.toastDiv.classList.remove("success");
-    }, 2000);
-  },
+        setTimeout(() => {
+            this.copyStatus.textContent = "";
+        }, 2000);
+    },
+    /**
+     * Show an error copyStatus message
+     * @param {string} [message="something went wrong"] - the message to show in the copyStatus
+     * @returns {void}
+     */
+    error(message = "something went wrong") {
+        this.copyStatus.style.color = "#F64A4A"
+        this.copyStatus.textContent = message;
+        password.value = ""
 
-  /**
-   * Show an error toast message
-   * @param {string} [message="something went wrong"] - the message to show in the toast
-   * @returns {void}
-   */
-  error(message = "something went wrong") {
-    this.toastDiv.classList.add("error");
-    this.toastDiv.innerHTML = message;
-
-    setTimeout(() => {
-      this.toastDiv.classList.remove("error");
-    }, 2000);
-  },
+        setTimeout(() => {
+            this.copyStatus.textContent = "";
+        }, 2000);
+    },
 };
 
+function updateSliderBackground() {
+    const min = slider.min;
+    const max = slider.max;
+    const value = slider.value;
+    const percentage = ((value - min) / (max - min)) * 100;
+
+    charLength.textContent = slider.value;
+
+    slider.style.background = `linear-gradient(to right, #A4FFAF ${percentage}%, #18171F ${percentage}%)`;
+}
 
 /**
  * Copy the generated password to the clipboard
  * @returns {void}
  */
 const copyPassword = () => {
-  if (!password.textContent.trim()) return toast.error("No password to copy");
-  navigator.clipboard.writeText(password.textContent);
-  toast.success("Password copied to clipboard!");
+    const copyStatus = document.getElementById("copy-status");
+    if (!password.value.trim()) return toast.error("no password provided")
+    navigator.clipboard.writeText(password.value);
+    toast.success("copied")
 };
 
 /**
- * 
+ *
  * @param {string} str - the string to get a random character from
  * @returns string
  */
@@ -81,16 +91,16 @@ const shufflePassword = (password) => password.sort(() => Math.random() - 0.5);
  * @returns {{passwordArray: Array<string>, availableCharacters: string, selectedOptions: number}}
  */
 const addCharactersToPool = (
-  charSet,
-  passwordArray,
-  availableCharacters,
-  selectedOptions
+    charSet,
+    passwordArray,
+    availableCharacters,
+    selectedOptions
 ) => {
-  passwordArray.push(getRandomChar(charSet));
-  availableCharacters += charSet;
-  selectedOptions++;
+    passwordArray.push(getRandomChar(charSet));
+    availableCharacters += charSet;
+    selectedOptions++;
 
-  return { passwordArray, availableCharacters, selectedOptions };
+    return {passwordArray, availableCharacters, selectedOptions};
 };
 
 /**
@@ -98,99 +108,96 @@ const addCharactersToPool = (
  * @returns {void}
  */
 const generatePassword = () => {
-  let passwordArray = [];
-  let availableCharacters = "";
-  let selectedOptions = 0;
+    if (slider.value === '0') return toast.error("select character length")
+    let passwordArray = [];
+    let availableCharacters = "";
+    let selectedOptions = 0;
 
-  // Add uppercase letters if checked
-  if (uppercaseCheck.checked) {
-    ({ passwordArray, availableCharacters, selectedOptions } = addCharactersToPool(
-      uppercaseLetters,
-      passwordArray,
-      availableCharacters,
-      selectedOptions
-    ));
-  }
+    // Add uppercase letters if checked
+    if (uppercaseCheck.checked) {
+        ({passwordArray, availableCharacters, selectedOptions} = addCharactersToPool(
+            uppercaseLetters,
+            passwordArray,
+            availableCharacters,
+            selectedOptions
+        ));
+    }
 
-  // Add lowercase letters if checked
-  if (lowercaseCheck.checked) {
-    ({ passwordArray, availableCharacters, selectedOptions } = addCharactersToPool(
-      lowercaseLetters,
-      passwordArray,
-      availableCharacters,
-      selectedOptions
-    ));
-  }
+    // Add lowercase letters if checked
+    if (lowercaseCheck.checked) {
+        ({passwordArray, availableCharacters, selectedOptions} = addCharactersToPool(
+            lowercaseLetters,
+            passwordArray,
+            availableCharacters,
+            selectedOptions
+        ));
+    }
 
-  // Add numbers if checked
-  if (numberCheck.checked) {
-    ({ passwordArray, availableCharacters, selectedOptions } = addCharactersToPool(
-      numbers,
-      passwordArray,
-      availableCharacters,
-      selectedOptions
-    ));
-  }
+    // Add numbers if checked
+    if (numberCheck.checked) {
+        ({passwordArray, availableCharacters, selectedOptions} = addCharactersToPool(
+            numbers,
+            passwordArray,
+            availableCharacters,
+            selectedOptions
+        ));
+    }
 
-  // Add symbols if checked
-  if (symbolCheck.checked) {
-    ({ passwordArray, availableCharacters, selectedOptions } = addCharactersToPool(
-      symbols,
-      passwordArray,
-      availableCharacters,
-      selectedOptions
-    ));
-  }
+    // Add symbols if checked
+    if (symbolCheck.checked) {
+        ({passwordArray, availableCharacters, selectedOptions} = addCharactersToPool(
+            symbols,
+            passwordArray,
+            availableCharacters,
+            selectedOptions
+        ));
+    }
 
-  // Handle password strength based on checked checkboxes
-  switch (selectedOptions) {
-    case 0:
-      toast.error("Check at least one checkbox");
-      return; // Exit if no checkbox is selected
-    case 1:
-      bars.forEach((bar) => (bar.className = ""));
-      bars[0].className = "very-weak-password";
-      strengthStatus.textContent = "Very weak";
-      break;
-    case 2:
-      bars.forEach((bar) => (bar.className = ""));
-      bars[0].className = "weak-password";
-      bars[1].className = "weak-password";
-      strengthStatus.textContent = "Weak";
-      break;
-    case 3:
-      bars.forEach((bar) => (bar.className = "medium-password"));
-      bars[3].className = "";
-      strengthStatus.textContent = "Medium";
-      break;
-    default:
-      bars.forEach((bar) => (bar.className = "strong-password"));
-      strengthStatus.textContent = "Strong";
-  }
+    if (selectedOptions > Number(slider.value)) return toast.error("not enough characters")
 
-  // Fill the rest of the password to match the desired length
-  const passwordLength = slider.value - passwordArray.length;
+    // Handle password strength based on checked checkboxes
+    switch (selectedOptions) {
+        case 0:
+            toast.error("Check at least one checkbox");
+            return; // Exit if no checkbox is selected
+        case 1:
+            bars.forEach((bar) => (bar.className = ""));
+            bars[0].className = "very-weak-password";
+            strengthStatus.textContent = "too weak";
+            break;
+        case 2:
+            bars.forEach((bar) => (bar.className = ""));
+            bars[0].className = "weak-password";
+            bars[1].className = "weak-password";
+            strengthStatus.textContent = "Weak";
+            break;
+        case 3:
+            bars.forEach((bar) => (bar.className = "medium-password"));
+            bars[3].className = "";
+            strengthStatus.textContent = "Medium";
+            break;
+        default:
+            bars.forEach((bar) => (bar.className = "strong-password"));
+            strengthStatus.textContent = "Strong";
+    }
 
-  for (let i = 0; i < passwordLength; i++) {
-    passwordArray.push(
-      availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
-    );
-  }
+    const passwordLength = slider.value - passwordArray.length;
 
-  // Shuffle the password characters for randomness
-  passwordArray = shufflePassword(passwordArray);
+    for (let i = 0; i < passwordLength; i++) {
+        passwordArray.push(
+            availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+        );
+    }
 
-  // Display the generated password
-  password.textContent = passwordArray.join("");
+    // Shuffle the password characters for randomness
+    passwordArray = shufflePassword(passwordArray);
+
+    // Display the generated password
+    password.value = passwordArray.join("");
 };
 
-// console.log(passwordInputGroup.children[0].children[0].checked)
-
 // event handlers
-
 copyBtn.addEventListener("click", copyPassword);
 
-slider.addEventListener("input", () => (charLength.textContent = slider.value));
+slider.addEventListener("input", updateSliderBackground);
 generateBtn.addEventListener("click", generatePassword);
-
-window.addEventListener("load", generatePassword);
