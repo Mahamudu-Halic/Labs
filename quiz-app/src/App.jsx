963fdb47db98eaf5d05703a9fb12d6bb1c515a7e
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-25 17:47:03 +0000

    assets added

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,6 +1,5 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ThemeToggler from "./components/ThemeToggler/ThemeToggler.jsx";
 
