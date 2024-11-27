4aab6d6da184b9d86a2d49927e4aa2178f9efe45
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-11-27 06:06:20 +0000

    moved question to a component

--- quiz-app/src/components/Question/Question.component.tsx
+++ quiz-app/src/components/Question/Question.component.tsx
@@ -1,9 +1,27 @@
-const Question = () => {
-    return (
-        <div>
-            question of the day
-        </div>
-    )
-}
+const Question = ({
+  currentQuestion,
+  questionNumber,
+  totalQuestions,
+}: {
+  currentQuestion: string;
+  questionNumber: number;
+  totalQuestions: number;
+}) => {
+  return (
+    <div className={"question-container"}>
+      <p className="question__number">
+        Question {questionNumber + 1} of {totalQuestions}
+      </p>
+      <h2 className={"question"}>{currentQuestion}</h2>
+      <input
+        type="range"
+        className={"question__progress_bar"}
+        max={totalQuestions}
+        value={questionNumber + 1}
+        min={1}
+      />
+    </div>
+  );
+};
 
 export default Question;
