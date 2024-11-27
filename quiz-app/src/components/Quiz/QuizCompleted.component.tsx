ae8d07d4c7418dedb3da9c72adcd6d0866ddcf35
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-27 14:53:40 +0000

    display score to user

---
+++ quiz-app/src/components/Quiz/QuizCompleted.component.tsx
@@ -0,0 +1,34 @@
+import {Data} from "../../../types.ts";
+import SelectedQuiz from "../Selected Quiz/SelectedQuiz.component.tsx";
+import MessageComponent from "../Message/Message.component.tsx";
+
+const QuizCompleted = ({title, icon, score, totalQuestions, setQuiz}: {
+    setQuiz: (value: Data | undefined) => void,
+    score: number,
+    totalQuestions: number,
+    title: string,
+    icon: string
+}) => {
+    const reset = () => {
+        sessionStorage.removeItem("score");
+        sessionStorage.removeItem("questionNumber");
+        sessionStorage.removeItem("hasCompleted");
+        sessionStorage.removeItem("quiz")
+        setQuiz(undefined)
+    }
+    return (
+        <div className={"quiz-completed"}>
+            <MessageComponent title={"Quiz Completed"} subtitle={"You scored..."} message={""}/>
+            <div className="results-container">
+                <div className={"score-container"}>
+                    <SelectedQuiz title={title} icon={icon}/>
+                    <h3 className="score">{score}</h3>
+                    <p className="totalScore">out of {totalQuestions}</p>
+                </div>
+                <button className={"submit__btn reset__btn"} onClick={reset}>Play Again</button>
+            </div>
+        </div>
+    )
+}
+
+export default QuizCompleted
