9b4125570857d04650e045ded6c4e26dac1170a4
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-11-27 06:09:46 +0000

    checks if user has unfinished quiz

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,11 +1,16 @@
 import HomepageComponent from "./components/Homepage/Homepage.component.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Data} from "../types.ts";
 import QuizComponent from "./components/Quiz/Quiz.component.tsx";
 import HeaderComponent from "./components/Header/Header.component.tsx";
+import getQuiz from "./utils/getQuiz.ts";
 
 function App() {
     const [quiz, setQuiz] = useState<Data | undefined>(undefined);
+
+    useEffect(() => {
+        getQuiz({callback: setQuiz, value: ""});
+    }, []);
     
     return (
         <div className="wrapper">
