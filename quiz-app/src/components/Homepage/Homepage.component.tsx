a89c30a6e2fef5b4c73752f0ce4cae24bbabcbc1
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-11-27 14:50:26 +0000

    message displayed passed to message component

--- quiz-app/src/components/Homepage/Homepage.component.tsx
+++ quiz-app/src/components/Homepage/Homepage.component.tsx
@@ -1,8 +1,6 @@
-import fetchData from "../../utils/fetchData.ts";
-
 import IconComponent from "../Icon/Icon.component.tsx";
 
-import { Data, DataResponse } from "../../../types.ts";
+import {Data} from "../../../types.ts";
 
 import jsSvg from "/assets/images/icon-js.svg";
 import htmlSvg from "/assets/images/icon-html.svg";
@@ -11,51 +9,48 @@ import accessibilitySvg from "/assets/images/icon-accessibility.svg";
 
 import "./homepage.styles.css";
 import getQuiz from "../../utils/getQuiz.ts";
+import MessageComponent from "../Message/Message.component.tsx";
 
 const Topics = [
-  {
-    label: "HTML",
-    icon: htmlSvg,
-  },
-  {
-    label: "CSS",
-    icon: cssSvg,
-  },
-  {
-    label: "JavaScript",
-    icon: jsSvg,
-  },
-  {
-    label: "Accessibility",
-    icon: accessibilitySvg,
-  },
+    {
+        label: "HTML",
+        icon: htmlSvg,
+    },
+    {
+        label: "CSS",
+        icon: cssSvg,
+    },
+    {
+        label: "JavaScript",
+        icon: jsSvg,
+    },
+    {
+        label: "Accessibility",
+        icon: accessibilitySvg,
+    },
 ];
 
-const HomepageComponent = ({ setQuiz }: { setQuiz: (value: Data) => void }) => {
-  return (
-    <div className="homepage">
-      <main>
-        <section className="welcome__message-container">
-          <h2 className="welcome__message-title">
-            Welcome to the <span>Frontend Quiz!</span>
-          </h2>
-          <p className="welcome__message">Pick a subject to get started.</p>
-        </section>
-        <div className="options">
-          {Topics.map((topic) => (
-            <button
-              key={topic.label}
-              className={`options__item`}
-              onClick={() => getQuiz({value: topic.label, callback:setQuiz})}
-            >
-              <IconComponent icon={topic.icon} customClassName={topic.label} />{" "}
-              {topic.label}
-            </button>
-          ))}
+const HomepageComponent = ({setQuiz}: { setQuiz: (value: Data) => void }) => {
+    return (
+        <div className="homepage">
+            <main>
+                <MessageComponent title={"Welcome to the"} subtitle={"Frontend Quiz!"}
+                                  message={"Pick a subject to get started."}/>
+                <div className="options">
+                    {Topics.map((topic) => (
+                        <button
+                            key={topic.label}
+                            className={`options__item`}
+                            onClick={() => getQuiz({value: topic.label, callback: setQuiz})}
+                        >
+                            <IconComponent icon={topic.icon} customClassName={topic.label}/>{" "}
+                            {topic.label}
+                        </button>
+                    ))}
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default HomepageComponent;
