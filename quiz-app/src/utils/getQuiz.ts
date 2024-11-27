69c17e26b9fdd738a0e7b9cae149dffee245d080
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-11-27 06:08:31 +0000

    get quiz from fetch data

---
+++ quiz-app/src/utils/getQuiz.ts
@@ -0,0 +1,21 @@
+import { Data, DataResponse } from "../../types";
+import fetchData from "./fetchData";
+
+const getQuiz = ({value, callback}: {value: string | "", callback: (value: Data) => void}) => {
+  const quiz = sessionStorage.getItem("quiz");
+
+  const response: DataResponse = fetchData(quiz || value);
+
+  if (response.status === "failed") {
+    console.log(response?.message);
+    return;
+  }
+
+  sessionStorage.setItem("quiz", quiz || value);
+
+  return (
+    response.status === "success" && response?.data && callback(response.data)
+  );
+};
+
+export default getQuiz
