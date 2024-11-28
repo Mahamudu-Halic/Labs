05ae25d2dd33a0285a85919f159997c6edb751fd
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-11-28 02:06:22 +0000

    tenary operator applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import HomepageComponent from "./components/Homepage/Homepage.component.tsx";
-import {useEffect, useState} from "react";
-import {Data} from "../types.ts";
+import { useEffect, useState } from "react";
+import { Data } from "../types.ts";
 import HeaderComponent from "./components/Header/Header.component.tsx";
 import getQuiz from "./utils/getQuiz.ts";
 import QuizContainer from "./components/Quiz/QuizContainer.component.tsx";
@@ -12,24 +12,22 @@ import QuizContainer from "./components/Quiz/QuizContainer.component.tsx";
  * @returns {JSX.Element} The JSX element representing the app.
  */
 function App() {
-    const [quiz, setQuiz] = useState<Data | undefined>(undefined);
+  const [quiz, setQuiz] = useState<Data | undefined>(undefined);
 
-    useEffect(() => {
-        getQuiz({callback: setQuiz, value: ""});
-    }, []);
+  useEffect(() => {
+    getQuiz({ callback: setQuiz, value: "" });
+  }, []);
 
-    return (
-        <div className="wrapper">
-            <HeaderComponent title={quiz?.title || ""} icon={quiz?.icon || ""}/>
-            {
-                !quiz && <HomepageComponent setQuiz={setQuiz}/>
-            }
-
-            {
-                quiz && <QuizContainer quiz={quiz} setQuiz={setQuiz}/>
-            }
-        </div>
-    )
+  return (
+    <div className="wrapper">
+      <HeaderComponent title={quiz?.title || ""} icon={quiz?.icon || ""} />
+      {!quiz ? (
+        <HomepageComponent setQuiz={setQuiz} />
+      ) : (
+        <QuizContainer quiz={quiz} setQuiz={setQuiz} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
