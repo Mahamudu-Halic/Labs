3bc034bf944c0bdfa7cabeb584bdcdaaab89126d
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-03 19:59:08 +0000

    form steps added

--- src/components/AuthForm/AuthForm.component.tsx
+++ src/components/AuthForm/AuthForm.component.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import "./authform.styles.css";
+import Form from "./Form.component";
+import Step from "./Step.component";
+import StepContainer from "./StepContainer.component";
 function AuthForm() {
-    return (
-        <div>
-            auth form
-        </div>
-    )
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
+  
+
+  return (
+    <div className="auth__form">
+      <StepContainer currentStep={currentStep}/>
+      <div className="auth__form-container">
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default AuthForm;
