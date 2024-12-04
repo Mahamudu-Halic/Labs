ca2ac4caacfa318b746c9e84a8cadaa0ef5b93e7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-04 08:20:33 +0000

    step one personal info components

---
+++ src/components/AuthForm/PersonalInfo/FormGroup/FormGroup.component.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { FormGroupType } from "../../../../types";
+
+const FormGroup = ({
+  label,
+  inputType,
+  placeholder,
+  value,
+  errorMsg,
+  onChange,
+}: FormGroupType) => {
+  return (
+    <div className="form-group">
+      <div className="form-group__label">
+        <label htmlFor={label.split(" ").join("")}>{label}</label>
+        {errorMsg && <p className="error__message">{errorMsg}</p>}
+      </div>
+      <input
+        className={errorMsg ? "error" : ""}
+        id={label.split(" ").join("")}
+        type={inputType}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default FormGroup;
---
+++ src/components/AuthForm/PersonalInfo/PersonalInfo.component.tsx
@@ -0,0 +1,55 @@
+import React, { ChangeEvent, useState } from "react";
+import Header from "../Header/Header.component";
+import { PersonalInfoType } from "../../../types";
+import FormGroup from "./FormGroup/FormGroup.component";
+
+const PersonalInfo = ({
+  userName,
+  userErrorMsg,
+  email,
+  emailErrorMsg,
+  phoneNumber,
+  phoneErrorMsg,
+  updateUserName,
+  updateEmail,
+  updatePhoneNumber,
+}: PersonalInfoType) => {
+  
+  return (
+    <div className="personal-info wrapper">
+      <Header
+        title="Personal Info"
+        description="Please provide your name, email address, and phone number."
+      />
+
+      <form>
+        <FormGroup
+          inputType="text"
+          label="Name"
+          placeholder="e.g. Stephen King"
+          value={userName}
+          onChange={updateUserName}
+          errorMsg={userErrorMsg}
+        />
+        <FormGroup
+          inputType="email"
+          label="Email address"
+          placeholder="e.g. john.doe@example.com"
+          value={email}
+          onChange={updateEmail}
+          errorMsg={emailErrorMsg}
+        />
+        <FormGroup
+          inputType="tel"
+          label="Phone number"
+          placeholder="e.g. +1 234 567 890"
+          value={phoneNumber}
+          onChange={updatePhoneNumber}
+          errorMsg={phoneErrorMsg}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default PersonalInfo;
