e287ee315b3d03a51882e9753f875cbb99431d3d
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-04 08:19:28 +0000

    header for step topics

---
+++ src/components/AuthForm/Header/Header.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = ({title, description}:{title: string, description: string}) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </header>
+  )
+}
+
+export default Header
