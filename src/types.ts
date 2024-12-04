b41762d93167e7ff2b82daec925f501330a14db7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-04 08:17:16 +0000

    types created

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+import { ChangeEvent } from "react";
+
+type FormOnChangeType = (event: ChangeEvent<HTMLInputElement>) => void;
+export type FormGroupType = {
+  label: string;
+  inputType: string;
+  placeholder: string;
+  value: string;
+  errorMsg: string;
+  onChange: FormOnChangeType;
+};
+
+export type UpdateFieldType = (
+  fieldName: string,
+  setState: React.Dispatch<React.SetStateAction<string>>
+) => (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+
+
+export type PersonalInfoType = {
+  userName: string;
+  email: string;
+  phoneNumber: string;
+  userErrorMsg: string;
+  emailErrorMsg: string;
+  phoneErrorMsg: string;
+  updateUserName: FormOnChangeType;
+  updateEmail: FormOnChangeType;
+  updatePhoneNumber: FormOnChangeType;}
