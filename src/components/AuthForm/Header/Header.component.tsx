528d9c91b3e7c2ccb9686133434620757629d156
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-12-09 10:42:54 +0000

    modal for resetting formadded

--- src/components/AuthForm/Header/Header.component.tsx
+++ src/components/AuthForm/Header/Header.component.tsx
@@ -1,12 +1,29 @@
-const Header = ({title, description, reset}: { title: string, description: string, reset?: () => void }) => {
+import {useState} from "react";
+
+const Header = ({title, description, reset}: { title: string, description: string, reset: () => void }) => {
+    const [showModal, setShowModal] = useState(false);
+    const toggleShowModal = () => setShowModal(prev => !prev)
+
+    const handleReset = () => {
+        reset();
+        toggleShowModal()
+    }
     return (
         <header>
-            <div>
+            <div className={"header__content"}>
                 <h1>{title}</h1>
                 <p>{description}</p>
             </div>
 
-            <button onClick={reset}>Reset</button>
+            <button onClick={toggleShowModal}>Reset</button>
+
+            {showModal && <div className={"header__reset-modal"}>
+                <p>Are you sure you want to reset the form</p>
+                <div className="confirm-button-container">
+                    <button className={"cancel-btn"} onClick={toggleShowModal}>No</button>
+                    <button onClick={handleReset}>Yes</button>
+                </div>
+            </div>}
         </header>
     )
 }
