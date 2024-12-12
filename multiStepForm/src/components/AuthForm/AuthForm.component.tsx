0e7745857f259509e3a81a09e15f4893df5fd6e4
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-12-12 10:33:23 +0000

    redux reducers to handle validations

--- multiStepForm/src/components/AuthForm/AuthForm.component.tsx
+++ multiStepForm/src/components/AuthForm/AuthForm.component.tsx
@@ -7,88 +7,63 @@ import ThankYou from "./ThankYou/ThankYou.component.tsx";
 
 import "./authform.styles.css";
 import {
-  nextStep,
-  prevStep,
-  selectCurrentStep,
-  selectFormData,
-  selectFormErrors,
-  selectIsComplete,
-  submitForm,
+    nextStep,
+    prevStep,
+    selectCurrentStep,
+    selectFormData,
+    selectFormErrors,
+    selectIsComplete,
+    submitForm,
 } from "../../features/Form/FormSlice.tsx";
-import { useAppDispatch } from "../../hooks/useAppDispatch.ts";
-import { useAppSelector } from "../../hooks/useAppSelector.ts";
-import { Errors, FormItems } from "../../types.ts";
+import {useAppDispatch} from "../../hooks/useAppDispatch.ts";
+import {useAppSelector} from "../../hooks/useAppSelector.ts";
+import {Errors, FormItems} from "../../types.ts";
 
 function AuthForm() {
-  const dispatch = useAppDispatch();
-  const formData: FormItems = useAppSelector(selectFormData);
-  const errors: Errors = useAppSelector(selectFormErrors);
-  const currentStep: number = useAppSelector(selectCurrentStep);
-  const isComplete = useAppSelector(selectIsComplete);
+    const dispatch = useAppDispatch();
+    const formData: FormItems = useAppSelector(selectFormData);
+    const errors: Errors = useAppSelector(selectFormErrors);
+    const currentStep: number = useAppSelector(selectCurrentStep);
+    const isComplete: boolean = useAppSelector(selectIsComplete);
 
-  // const handleStepValidation = () => {
-  //     if (currentStep === 3) {
-  //
-  //         if (Object.values(newErrors).every((error) => error === "")) {
-  //             setIsComplete(true);
-  //             reset();
-  //             return;
-  //         } else {
-  //             setErrors((prevErrors) => ({...prevErrors, summaryErr: "please complete the form before submitting"}));
-  //             return;
-  //         }
-  //     }
-  //
-  //     if (formIsValid) setCurrentStep(() => nextStep(currentStep, totalSteps));
-  // };
+    return (
+        <div className="auth__form">
+            <StepContainer currentStep={currentStep} complete={isComplete}/>
 
-  return (
-    <div className="auth__form">
-      <StepContainer currentStep={currentStep} complete={isComplete} />
+            <div className="auth__form-container">
+                {!isComplete && (
+                    <>
+                        {currentStep === 0 && <PersonalInfo {...formData} {...errors} />}
+                        {currentStep === 1 && (<Plan {...formData} planErr={errors.planErr}/>)}
+                        {currentStep === 2 && <AddOns {...formData} />}
+                        {currentStep === 3 && (<Summary {...formData} summaryErr={errors.summaryErr}/>)}
+                    </>
+                )}
 
-      <div className="auth__form-container">
-        {!isComplete && (
-          <>
-            {currentStep === 0 && <PersonalInfo {...formData} {...errors} />}
-            {currentStep === 1 && (
-              <Plan {...formData} planErr={errors.planErr} />
-            )}
-            {currentStep === 2 && <AddOns {...formData} />}
-            {currentStep === 3 && (
-              <Summary {...formData} summaryErr={errors.summaryErr} />
-            )}
-          </>
-        )}
+                {isComplete && <ThankYou/>}
 
-        {isComplete && <ThankYou />}
-
-        {!isComplete && (
-          <div className="auth__form-button-container">
-            {currentStep !== 0 && (
-              <button
-                onClick={() => dispatch(prevStep())}
-                className="auth__form-prev-button"
-              >
-                go back
-              </button>
-            )}
-            <button
-              onClick={() =>
-                currentStep === 3
-                  ? dispatch(submitForm())
-                  : dispatch(nextStep())
-              }
-              className={`auth__form-button ${
-                currentStep === 3 ? "confirm" : ""
-              }`}
-            >
-              {currentStep === 3 ? "Confirm" : "Next Step"}
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                {!isComplete && (
+                    <div className="auth__form-button-container">
+                        {currentStep !== 0 && (
+                            <button onClick={() => dispatch(prevStep())} className="auth__form-prev-button">go
+                                back</button>)}
+                        <button
+                            onClick={() =>
+                                currentStep === 3
+                                    ? dispatch(submitForm())
+                                    : dispatch(nextStep())
+                            }
+                            className={`auth__form-button ${
+                                currentStep === 3 ? "confirm" : ""
+                            }`}
+                        >
+                            {currentStep === 3 ? "Confirm" : "Next Step"}
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default AuthForm;
--- multiStepForm/src/features/Form/FormSlice.tsx
+++ multiStepForm/src/features/Form/FormSlice.tsx
@@ -1,133 +1,138 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Errors, FormItems } from "../../types.ts";
-import { initialErrorValues, initialValues } from "../../constant.ts";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {Errors, FormItems} from "../../types.ts";
+import {initialErrorValues, initialValues} from "../../constant.ts";
 import formFieldValidation from "../../utils/form.field.validation.ts";
-import { RootState } from "../../store.ts";
+import {RootState} from "../../store.ts";
 
 interface FormState {
-  formData: FormItems;
-  errors: Errors;
+    formData: FormItems;
+    errors: Errors;
 }
 
 const initialState: FormState = {
-  formData: initialValues,
-  errors: initialErrorValues,
+    formData: initialValues,
+    errors: initialErrorValues,
 };
 
 const formSlice = createSlice({
-  name: "form",
-  initialState,
-  reducers: {
-    updateForm(
-      state: FormState,
-      action: PayloadAction<{
-        fieldToUpdate: Partial<FormItems>;
-        field?: keyof FormItems;
-      }>
-    ) {
-      const { fieldToUpdate, field } = action.payload;
-      state.formData = { ...state.formData, ...fieldToUpdate };
+    name: "form",
+    initialState,
+    reducers: {
+        updateForm(
+            state: FormState,
+            action: PayloadAction<{
+                fieldToUpdate: Partial<FormItems>;
+                field?: keyof FormItems;
+            }>
+        ) {
+            const {fieldToUpdate, field} = action.payload;
+            state.formData = {...state.formData, ...fieldToUpdate};
 
-      if (field) {
-        state.errors = {
-          ...state.errors,
-          [`${field}Err`]: formFieldValidation(
-            field,
-            fieldToUpdate[field] as string
-          ),
-        };
-      }
+            if (field) {
+                state.errors = {
+                    ...state.errors,
+                    [`${field}Err`]: formFieldValidation(
+                        field,
+                        fieldToUpdate[field] as string
+                    ),
+                };
+            }
+        },
+
+        goToStep(state: FormState, action: PayloadAction<{ step: number }>) {
+            const {step} = action.payload;
+            if (step >= 0 && step <= 4) {
+                state.formData.currentStep = step;
+            }
+        },
+
+        nextStep(state: FormState) {
+            const {formData} = state;
+            if (formData.currentStep === 0) {
+                const newErrors = {
+                    nameErr: formFieldValidation("name", formData.name),
+                    emailErr: formFieldValidation("email", formData.email),
+                    phoneNumberErr: formFieldValidation(
+                        "phoneNumber",
+                        formData.phoneNumber
+                    ),
+                };
+                state.errors = {...state.errors, ...newErrors};
+                if (!newErrors.emailErr && !newErrors.nameErr && !newErrors.phoneNumberErr) {
+                    state.formData.currentStep++;
+                    return
+                }
+
+            }
+
+            if (formData.currentStep === 1) {
+                const newError = {
+                    planErr: formFieldValidation("plan", `${formData.plan}`),
+                };
+
+                state.errors = {...state.errors, ...newError};
+                if (!newError.planErr) {
+                    state.formData.currentStep++;
+                    return
+                }
+            }
+
+            state.formData.currentStep++;
+        },
+
+        prevStep(state: FormState) {
+            if (state.formData.currentStep > 0) {
+                state.formData.currentStep--;
+            }
+        },
+
+        resetForm(state: FormState) {
+            state.formData = initialValues;
+            state.errors = initialErrorValues;
+        },
+
+        submitForm(state: FormState) {
+            const {formData} = state;
+            const errors = {
+                nameErr: formFieldValidation("name", formData.name),
+                emailErr: formFieldValidation("email", formData.email),
+                phoneNumberErr: formFieldValidation(
+                    "phoneNumber",
+                    formData.phoneNumber
+                ),
+                planErr: formFieldValidation("plan", `${formData.plan}`),
+            };
+            state.errors = {...state.errors, ...errors};
+            if (
+                errors.emailErr ||
+                errors.nameErr ||
+                errors.phoneNumberErr ||
+                errors.planErr
+            ) {
+                state.errors.summaryErr = "please complete the form before submitting";
+                return;
+            }
+
+            formData.isComplete = true;
+        },
     },
-
-    goToStep(state: FormState, action: PayloadAction<{ step: number }>) {
-      const { step } = action.payload;
-      if (step >= 0 && step <= 4) {
-        state.formData.currentStep = step;
-      }
-    },
-
-    nextStep(state: FormState) {
-      let isValid = true;
-      const { formData } = state;
-      if (formData.currentStep === 0) {
-        const newErrors = {
-          nameErr: formFieldValidation("name", formData.name),
-          emailErr: formFieldValidation("email", formData.email),
-          phoneNumberErr: formFieldValidation(
-            "phoneNumber",
-            formData.phoneNumber
-          ),
-        };
-        state.errors = { ...state.errors, ...newErrors };
-        if (newErrors.emailErr && newErrors.nameErr && newErrors.phoneNumberErr)
-          isValid = false;
-      }
-
-      if (formData.currentStep === 1) {
-        const newError = {
-          planErr: formFieldValidation("plan", `${formData.plan}`),
-        };
-
-        state.errors = { ...state.errors, ...newError };
-        if (newError.planErr) isValid = false;
-      }
-      if (isValid) {
-        state.formData.currentStep++;
-      }
-    },
-
-    prevStep(state: FormState) {
-      if (state.formData.currentStep > 0) {
-        state.formData.currentStep--;
-      }
-    },
-
-    resetForm(state: FormState) {
-      state.formData = initialValues;
-      state.errors = initialErrorValues;
-    },
-
-    submitForm(state: FormState) {
-      const { formData } = state;
-      const errors = {
-        nameErr: formFieldValidation("name", formData.name),
-        emailErr: formFieldValidation("email", formData.email),
-        phoneNumberErr: formFieldValidation(
-          "phoneNumber",
-          formData.phoneNumber
-        ),
-        planErr: formFieldValidation("plan", `${formData.plan}`),
-      };
-      state.errors = { ...state.errors, ...errors };
-      if (
-        errors.emailErr ||
-        errors.nameErr ||
-        errors.phoneNumberErr ||
-        errors.planErr
-      ) {
-        state.errors.summaryErr = "please complete the form before submitting";
-        return;
-      }
-
-      formData.isComplete = true;
-    },
-  },
 });
 
 export const {
-  updateForm,
-  resetForm,
-  nextStep,
-  prevStep,
-  goToStep,
-  submitForm,
+    updateForm,
+    resetForm,
+    nextStep,
+    prevStep,
+    goToStep,
+    submitForm,
+    validateStep
 } = formSlice.actions;
 
 export const selectFormData = (state: RootState) => state.form.formData;
 export const selectFormErrors = (state: RootState) => state.form.errors;
 export const selectCurrentStep = (state: RootState) =>
-  state.form.formData.currentStep;
+    state.form.formData.currentStep;
 export const selectIsComplete = (state: RootState) =>
-  state.form.formData.isComplete;
+    state.form.formData.isComplete;
 
 export default formSlice.reducer;
