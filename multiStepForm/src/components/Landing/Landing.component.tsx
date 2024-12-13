52469423eff752fb18ca3e4e7b112968a32dd9d7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-13 02:02:53 +0000

    landing page edits

--- multiStepForm/src/components/Landing/Landing.component.tsx
+++ multiStepForm/src/components/Landing/Landing.component.tsx
@@ -1,59 +1,30 @@
 import "./landing.styles.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const goToForm = () => {
-        navigate("/auth-form");
-    };
+  const goToForm = () => {
+    navigate("/auth-form");
+  };
 
-    return (
-        <div className={"landing"}>
-
-            <div className="landing-hero">
-
-                <div className="landing-hero__content">
-                    <h1>Lorem <span>Gaming,</span></h1>
-                    <p>
-                        One of the popular online gaming service providers in US has recently launched a new
-                        online gaming experience that customers can subscribe to for access to various features
-                    </p>
-                    <button className={"landing__button"} onClick={goToForm}>
-                        Get Started
-                    </button>
-                </div>
-                <div className="landing-hero__image">
-
-                    <img
-                        src="/assets/images/fill-form.svg"
-                        alt={"info"}
-                    />
-                </div>
-            </div>
-
-            <section>
-                <h2>What to expert from the form 👇</h2>
-                <div className="landing__image-container">
-                    <img
-                        className={"landing__image"}
-                        src="/assets/images/info.png"
-                        alt={"info"}
-                    />
-                    <img
-                        className={"landing__image"}
-                        src="/assets/images/plan.png"
-                        alt={"plan"}
-                    />
-                    <img
-                        className={"landing__image"}
-                        src="/assets/images/add-ons.png"
-                        alt={"add-ons"}
-                    />
-                </div>
-            </section>
-        </div>
-    );
+  return (
+    <div className={"landing"}>
+      <div className="overlay"></div>
+      <div className="content">
+        <span>Welcome to</span>
+        <h1 className="title">Lorem Gaming</h1>
+        <p className="description">
+          One of the popular online gaming service providers in US has recently
+          launched a new online gaming experience that customers can subscribe
+          to for access to various features
+        </p>
+        <button onClick={goToForm} className="button">
+          Get started
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
