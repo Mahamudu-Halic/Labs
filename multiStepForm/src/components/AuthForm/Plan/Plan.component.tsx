6566e61579ddc5955353b1f87c1113333a60f6fb
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-13 00:00:54 +0000

    reducers for updateform and nextstep updated

--- multiStepForm/src/components/AuthForm/Plan/Plan.component.tsx
+++ multiStepForm/src/components/AuthForm/Plan/Plan.component.tsx
@@ -1,33 +1,40 @@
 import Header from "../Header/Header.component.tsx";
 import Cards from "./Card/Cards.component.tsx";
 import TimeFrameToogler from "./TimeFrameToggler/TimeFrameToggler.component.tsx";
-import {PlanType} from "../../../types.ts";
-import {updateForm} from "../../../features/Form/FormSlice.tsx";
-import {useAppDispatch} from "../../../hooks/useAppDispatch.ts";
+import { PlanType } from "../../../types.ts";
+import { updateForm } from "../../../features/Form/FormSlice.tsx";
+import { useAppDispatch } from "../../../hooks/useAppDispatch.ts";
 
-const Plan = ({plan, timeFrame, planErr}: PlanType) => {
-    const dispatch = useAppDispatch()
-    return (
-        <div className="plan wrapper">
-            <Header
-                description="You have the option of monthly or yearly billing."
-                title="Select your plan"
-            />
+const Plan = ({ plan, timeFrame, planErr }: PlanType) => {
+  const dispatch = useAppDispatch();
+  return (
+    <div className="plan wrapper">
+      <Header
+        description="You have the option of monthly or yearly billing."
+        title="Select your plan"
+      />
 
-            {planErr && <p className={"error"}>{planErr}</p>}
-            <Cards
-                plan={plan}
-                updateForm={({title, price}) => dispatch(updateForm({fieldToUpdate: {plan: {title, price}}}))
-                }
-                timeFrame={timeFrame}
-            />
-            <TimeFrameToogler
-                timeFrame={timeFrame}
-                updateForm={(value) => dispatch(updateForm({fieldToUpdate: {timeFrame: value}}))}
-            />
-
-        </div>
-    );
+      {planErr && <p className={"error"}>{planErr}</p>}
+      <Cards
+        plan={plan}
+        updateForm={({ title, price }) =>
+          dispatch(
+            updateForm({
+              fieldToUpdate: { plan: { title, price } },
+              field: "plan",
+            })
+          )
+        }
+        timeFrame={timeFrame}
+      />
+      <TimeFrameToogler
+        timeFrame={timeFrame}
+        updateForm={(value) =>
+          dispatch(updateForm({ fieldToUpdate: { timeFrame: value } }))
+        }
+      />
+    </div>
+  );
 };
 
 export default Plan;
