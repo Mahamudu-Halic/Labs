a73908db34dac371d722368298461388a60dd1bb
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-12-13 00:38:47 +0000

    renamed from debounce to useDebounce

--- multiStepForm/src/utils/debounce.ts
+++ multiStepForm/src/utils/debounce.ts
@@ -1,15 +1,15 @@
-const debounce = (func: (...args: any[]) => void, timeout = 300) => {
-    let timer: NodeJS.Timeout | undefined;
+const useDebounce = (func: (...args: any[]) => void, timeout = 300) => {
+  let timer: NodeJS.Timeout | undefined;
 
-    return (...args: any[]) => {
-        // Clear the existing timer
-        clearTimeout(timer);
+  return (...args: any[]) => {
+    // Clear the existing timer
+    clearTimeout(timer);
 
-        // Set a new timer
-        timer = setTimeout(() => {
-            func(...args);
-        }, timeout);
-    };
+    // Set a new timer
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
 };
 
-export default debounce;
+export default useDebounce;
