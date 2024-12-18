2b16cc5b5dadedd09830632d781013b2f0a334c8
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-12-18 16:42:47 +0000

    button component to render custom button based on variant

--- invoice-app/src/components/ui/button/button.tsx
+++ invoice-app/src/components/ui/button/button.tsx
@@ -1,29 +1,31 @@
-import { ReactNode, HTMLAttributes } from "react";
+import {ReactNode, HTMLAttributes} from "react";
+import styles from "./button.module.css"
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  children: ReactNode;
-  variant?: "default" | "primary" | "secondary" | "danger";
-  radius?:
-    | "rounded"
-    | "rounded-sm"
-    | "rounded-md"
-    | "rounded-lg"
-    | "rounded-full";
+    children: ReactNode;
+    variant?: "default" | "primary" | "secondary" | "tertiary" | "danger";
+    radius?:
+        | "rounded"
+        | "rounded-sm"
+        | "rounded-md"
+        | "rounded-lg"
+        | "rounded-full";
 }
-const Button = ({
-  children,
-  radius = "rounded",
-  variant = "default",
-  className,
-  ...props
-}: ButtonProps) => {
-  const combinedClassName = `button ${variant} ${radius} ${className}`.trim();
 
-  return (
-    <button className={combinedClassName} {...props}>
-      {children}
-    </button>
-  );
+const Button = ({
+                    children,
+                    radius = "rounded",
+                    variant = "default",
+                    className,
+                    ...props
+                }: ButtonProps) => {
+    const combinedClassName = `${styles.button} ${styles[variant]} ${styles[radius]} ${className || ""}`.trim();
+
+    return (
+        <button className={combinedClassName} {...props}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
