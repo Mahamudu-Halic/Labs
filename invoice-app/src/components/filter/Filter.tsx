29a61d5e25e40c31564a64d96f251a076ad3fbcb
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-12-18 16:44:36 +0000

    filter component to filter invoice

---
+++ invoice-app/src/components/filter/Filter.tsx
@@ -0,0 +1,31 @@
+import "./filter.styles.css";
+import Button from "../ui/button/button.tsx";
+import dropdownIcon from "../../assets/images/icon-arrow-down.svg";
+import Icon from "../ui/icon/Icon.tsx";
+import Dropdown from "../ui/dropdown/Dropdown.tsx";
+import { useState } from "react";
+
+const Filter = () => {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
+  const handleDropdownToggle = () => {
+    setShowDropdown((prev) => !prev);
+  };
+  return (
+    <div className={"filter"}>
+      <Button onClick={handleDropdownToggle}>
+        Filter by status{" "}
+        <Icon icon={dropdownIcon} description={"dropdown arrow"} />
+      </Button>
+
+      {showDropdown && (
+        <>
+          <div className="overlay" onClick={handleDropdownToggle}></div>
+          <Dropdown options={["halic", "hafiz", "adams"]} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Filter;
