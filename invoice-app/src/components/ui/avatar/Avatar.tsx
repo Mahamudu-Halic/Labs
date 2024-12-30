7ff7751b54725238efa53bbe528010f72cb664c9
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2024-12-30 10:51:42 +0000

    interactive avatar component

--- invoice-app/src/components/ui/avatar/Avatar.tsx
+++ invoice-app/src/components/ui/avatar/Avatar.tsx
@@ -1,7 +1,9 @@
 import Button from "../button/button.tsx";
 import Icon from "../icon/Icon.tsx";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState } from "react";
 import styles from "./avatar.module.css";
+import Text from "../typography/text/Text.tsx";
+import Headline from "../typography/headline/Headline.tsx";
 
 interface AvatarProps extends HTMLAttributes<HTMLButtonElement> {
   image: string;
@@ -21,10 +23,38 @@ const Avatar = ({
   className,
 }: AvatarProps) => {
   const combinedClassName = `${styles.avatar} ${className ?? ""}`.trim();
+
+  const [showProfile, setShowProfile] = useState<boolean>(false);
+
+  const handleToggleProfile = () => setShowProfile((prev) => !prev);
+
   return (
-    <Button radius={radius} className={combinedClassName}>
-      <Icon icon={image} description={"profile"} radius={radius} size={size} />
-    </Button>
+    <div className={`${styles.avatar__container}`}>
+      {showProfile && (
+        <>
+          <div className={styles.overlay} onClick={handleToggleProfile} />
+          <div className={styles.profile}>
+            <Headline variant={"h3"}>Mahamudu Halic</Headline>
+            <Text>halic.mahamudu@amalitech.com</Text>
+            <Button variant={"danger"} radius={"rounded-md"}>
+              logout
+            </Button>
+          </div>
+        </>
+      )}
+      <Button
+        radius={radius}
+        className={combinedClassName}
+        onClick={handleToggleProfile}
+      >
+        <Icon
+          icon={image}
+          description={"profile"}
+          radius={radius}
+          size={size}
+        />
+      </Button>
+    </div>
   );
 };
 
