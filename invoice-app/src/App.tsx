68f7a0c5fe41186169afc2c079bc0d19bc251816
Mahamudu-Halic <halic.mahamudu@amalitech.com>
2025-01-03 13:31:11 +0000

    toaster added

--- invoice-app/src/App.tsx
+++ invoice-app/src/App.tsx
@@ -9,8 +9,7 @@ import ViewInvoice from "./components/view-invoice/ViewInvoice.tsx";
 import { useEffect } from "react";
 import { fetchInvoices } from "./features/invoice/invoice.slice.ts";
 import { useAppDispatch } from "./hooks/useRedux.ts";
-
-// import { Toaster } from "sonner";
+import { Toaster } from "sonner";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -21,7 +20,7 @@ function App() {
 
   return (
     <div className={"app"}>
-      {/*<Toaster position={"top-right"} />*/}
+      <Toaster position={"top-right"} />
       <Sidebar />
       <div className={"content"}>
         <Routes>
